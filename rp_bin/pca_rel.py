eed89c9016e38c83f9b3bc7e81d108ceda2e368a
rkwalters <rwalters@broadinstitute.org>
2015-07-14 15:45:45 -0400

    executable, arguments, defaults

--- rp_bin/pca_rel.py
+++ rp_bin/pca_rel.py
@@ -13,38 +13,71 @@ import argparse
 primusx = ""
 flashpcax = ""
 rscriptx = ""
-
+pcadir = ""
 
 print '...Parsing arguments...' 
 # parse arguments
 parser = argparse.ArgumentParser(prog='pca_rel.py',
                                  formatter_class=lambda prog:
                                  argparse.ArgumentDefaultsHelpFormatter(prog, max_help_position=40))
-#...#
-                                 
-# plink_ex
-# primus_ex
-# rscript_ex
+parser.add_argument('--bfile', 
+                    type=str,
+                    metavar='FILESTEM',
+                    help='file stem for input plink bed/bim/fam',
+                    required=True)
+parser.add_argument('--out',
+                    type=str,
+                    metavar='OUTNAME',
+                    help='base name for output; recommend 4 character stem to match ricopili',
+                    required=True)
+parser.add_argument('--rel-deg',
+                    type=int,
+                    metavar='INT',
+                    help='relatedness degree threshold for defining \"unrelated\" set',
+                    required=False,
+                    default=3)
+parser.add_argument('--npcs',
+                    type=int,
+                    metavar='INT',
+                    help='number of principal components to compute',
+                    required=False,
+                    default=10)
+parser.add_argument('--pcadir',
+                    type=str,
+                    metavar='DIRNAME',
+                    help='name for PCA output directory, defaults to pca_imus_OUTNAME',
+                    required=False)
+# parser.add_argument('--plink-ex',
+#                    type=str,
+#                    metavar='PATH',
+#                    help='path to plink executable, read from ~/ricopili.conf if unspecified',
+#                    required=False)
+parser.add_argument('--rscript-ex',
+                    type=str,
+                    metavar='PATH',
+                    help='path to Rscript executable, tries reading from PATH if unspecified',
+                    required=False,
+                    default=spawn.find_executable("Rscript"))
+parser.add_argument('--primus-ex',
+                    type=str,
+                    metavar='PATH',
+                    help='path to PRIMUS executable',
+                    required=False,
+                    default=os.environ['HOME']+"/PRIMUS_v1.8.0/bin/run_PRIMUS.pl")
+parser.add_argument('--flashpca-ex',
+                    type=str,
+                    metavar='PATH',
+                    help='path to flashpca executable',
+                    required=False,
+                    default="/humgen/atgu1/fs03/shared_resources/shared_software/bin/flashpca")
 
-
-# plink file stem
-# name
-# pcadir
-# npca
-
-
-
+args, pass_through_args = parser.parse_known_args()
 
 #set remaining defaults#
-if flashpcax == "":
-    flashpcax = "/humgen/atgu1/fs03/shared_resources/shared_software/bin/flashpca"
-    
-if primusx == "":
-    primusx = "~/PRIMUS_v1.8.0/bin/run_PRIMUS.pl"
-    
-if rscriptx =="":
-    rscriptx = spawn.find_executable("Rscript") # check success later
- 
+if pcadir == "":
+    pcadir = 'pca_imus_' + args.out
+# mem?
+
  
 
 print '...reading ricopili config file...'
@@ -68,17 +101,22 @@ if rscriptx == None:
     raise AssertionError('Unable to find Rscript in search path')
     
 # file exists
-assert os.path.isfile(primusx), 'PRIMUS not found at ' + primusx
-assert os.path.isfile(flashpcax), 'FlashPCA not found at ' + flashpcax
-assert os.path.isfile(plinkx), 'Plink not found at ' + plinkx
-assert os.path.isfile(rscriptx), 'Rscript not found at ' + rscriptx
+assert os.path.isfile(args.primus_ex), "PRIMUS not found at %r" % args.primus_ex
+assert os.path.isfile(args.flashpca_ex), "FlashPCA not found at %r" % args.flashpca_ex
+assert os.path.isfile(plinkx), "Plink not found at %r" % plinkx
+assert os.path.isfile(args.rscript_ex), "Rscript not found at %r" % args.rscript_ex
 
 # file executable
-assert os.access(primusx, os.X_OK), 'FlashPCA not executable (' + primusx + ')'
-assert os.access(flashpcax, os.X_OK), 'FlashPCA not executable (' + flashpcax + ')'
+assert os.access(args.primus_ex, os.X_OK), 'FlashPCA not executable (' + args.primus_ex + ')'
+assert os.access(args.flashpca_ex, os.X_OK), 'FlashPCA not executable (' + args.flashpca_ex + ')'
 assert os.access(plinkx, os.X_OK), 'Plink not executable (' + plinkx + ')'
-assert os.access(rscriptx, os.X_OK), 'Rscript not executable (' + rscriptx + ')'
+assert os.access(args.rscript_ex, os.X_OK), 'Rscript not executable (' + args.rscript_ex + ')'
 
+
+
+print '############'
+print 'Begin!'
+print '############\n'
 
 print '...Computing IMUS (unrelated) set...'
 
