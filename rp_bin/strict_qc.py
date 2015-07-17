7943f9b5f53c92f403e9c669a6359496b2f7e29f
rkwalters <rwalters@broadinstitute.org>
2015-07-17 19:41:15 -0400

    finish switching ld regions to file input, add extra args

--- rp_bin/strict_qc.py
+++ rp_bin/strict_qc.py
@@ -71,19 +71,6 @@ parser.add_argument('--mind-th',
                     help='individual missingness threshold',
                     required=False,
                     default=0.95)
-parser.add_argument('--keep-indels',
-                    action='store_true',
-                    help='do not remove indels, i.e. variants with alleles I, D, -, or multiple bases')                    
-parser.add_argument('--extra-ld-regions',
-                    nargs='?',
-                    metavar='FILE',
-                    const='price_2008_ld_regions.txt',
-                    default=None,
-                    help='exclude LD regions other than than the MHC and the \
-                    chr 8 inversion. If FILE specified, \
-                    each line should have 3 columns indicating chromosome, \
-                    starting base pair, and end base pair of region to exclude. \
-                    If no file, uses regions from Price et al. (2008, AJHG).')
 parser.add_argument('--maf-th',
                     type=float,
                     metavar='FLOAT',
@@ -120,6 +107,28 @@ parser.add_argument('--ld-wind',
 #                     help='LD pruning window movement rate',
 #                     required=False,
 #                     default=100)
+parser.add_argument('--keep-indels',
+                    action='store_true',
+                    help='do not remove indels, i.e. variants with alleles I, D, -, or multiple bases')
+parser.add_argument('--keep-strand-ambiguous',
+                    action='store_true',
+                    help='do not remove strand ambiguous SNPs (i.e. A/T or G/C)')
+parser.add_argument('--keep-mhc',
+                    action='store_true',
+                    help='do not remove MHC region (chr 6: 25-35 Mb)')   
+parser.add_argument('--keep-chr8inv',
+                    action='store_true',
+                    help='do not remove chr. 8 inversion region (chr 8: 7-13 Mb)')
+parser.add_argument('--extra-ld-regions',
+                    nargs='?',
+                    metavar='FILE',
+                    const='price_2008_ld_regions.txt',
+                    default=None,
+                    help='exclude LD regions other than than the MHC and the \
+                    chr 8 inversion. If FILE specified, \
+                    each line should have 3 columns indicating chromosome, \
+                    starting base pair, and end base pair of region to exclude. \
+                    If no file, uses regions from Price et al. (2008, AJHG).')
 parser.add_argument('--all-chr',
                     action='store_true',
                     help='keep all chromosomes, instead of autosomes only')
@@ -137,6 +146,10 @@ print 'Using settings:'
 print '--bfile '+args.bfile
 print '--out '+args.out
 print '--mind-th '+str(args.mind_th)
+print '--keep-indels '+str(args.keep_indels)
+print '--keep-strand-ambiguous '+str(args.keep_strand_ambiguous)
+print '--keep-mhc '+str(args.keep_mhc)
+print '--keep-chr8inv '+str(args.keep_chr8inv)
 print '--keep-indels '+str(args.keep_indels)
 print '--extra-ld-regions '+str(args.extra_ld_regions)
 print '--maf-th '+str(args.maf_th)
@@ -232,14 +245,23 @@ subprocess.check_call([str(plinkx),
 print '\n...Finding indels, strand ambiguous SNPs, and long LD regions...'
 #############
 
-### get strand ambi list, mhc/etc list
+### define indel alleles
+indels = ['i','d','-']
+
+### prep extra long LD regions
+if args.extra_ld_regions != None:
+    ld_reg = []
+    with open(args.extra_ld_regions, 'r') as f:
+        for line in f:
+            ld_reg.append((line.split()))
+
+
+### prep bim input, snp exclusion output
 bim_in_nam = args.bfile + '.bim'
 snpout_nam = args.out + '.exclude_snps.txt'
 
 snp_in = open(bim_in_nam, 'r')
 snp_out = open(snpout_nam, 'w')
-
-indels = ['i','d','-']
 
 for line in snp_in:
     (chrom, snp, cm, bp, a1, a2) = line.split()
@@ -255,64 +277,28 @@ for line in snp_in:
         elif any(a == a1l for a in indels) or any(a == a2l for a in indels):
             snp_out.write(snp + ' indel_allele\n')
    
-    if (a1l=='a') and (a2l=='t'):
-        snp_out.write(snp + ' strand_ambiguous\n')
-    elif (a1l=='t') and (a2l=='a'):
-        snp_out.write(snp + ' strand_ambiguous\n')
-    elif (a1l=='g') and (a2l=='c'):
-        snp_out.write(snp + ' strand_ambiguous\n')
-    elif (a1l=='c') and (a2l=='g'):
-        snp_out.write(snp + ' strand_ambiguous\n')
+   if no args.keep_strand_ambiguous:
+        if (a1l=='a') and (a2l=='t'):
+            snp_out.write(snp + ' strand_ambiguous\n')
+        elif (a1l=='t') and (a2l=='a'):
+            snp_out.write(snp + ' strand_ambiguous\n')
+        elif (a1l=='g') and (a2l=='c'):
+            snp_out.write(snp + ' strand_ambiguous\n')
+        elif (a1l=='c') and (a2l=='g'):
+            snp_out.write(snp + ' strand_ambiguous\n')
+
+    if not args.keep_mhc:    
+        if (chrom==6) and (bp > 25000000) and (bp < 35000000):
+            snp_out.write(snp + ' mhc_region\n')
+            
+    if not args.keep_chr8inv:
+        elif (chrom==8) and (bp > 7000000) and (bp < 13000000):
+            snp_out.write(snp + ' chr8inv_region\n')
         
-    if (chrom==6) and (bp > 25000000) and (bp < 35000000):
-        snp_out.write(snp + ' mhc_region\n')
-    elif (chrom==8) and (bp > 7000000) and (bp < 13000000):
-        snp_out.write(snp + ' chr8inv_region\n')
-    elif args.extra_ld_regions:
-        if (chrom==1) and (bp > 48000000) and (bp < 52000000):
+    elif args.extra_ld_regions != None:
+        if any([chrom==int(ld_chr) and int(ld_bp_start) < bp < int(ld_bp_end) for (ld_chr, ld_bp_start, ld_bp_end) in ld_reg]):
             snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==2) and (bp > 86000000) and (bp < 101000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==2) and (bp > 134000000) and (bp < 138000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==2) and (bp > 183000000) and (bp < 190000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==3) and (bp > 47000000) and (bp < 50000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==3) and (bp > 83000000) and (bp < 87000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==3) and (bp > 89000000) and (bp < 98000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==5) and (bp > 44000000) and (bp < 51000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==5) and (bp > 98000000) and (bp < 101000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==5) and (bp > 129000000) and (bp < 132000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==5) and (bp > 135000000) and (bp < 139000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==6) and (bp > 57000000) and (bp < 64000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==6) and (bp > 140000000) and (bp < 143000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==7) and (bp > 55000000) and (bp < 66000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==8) and (bp > 43000000) and (bp < 50000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==8) and (bp > 112000000) and (bp < 115000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==10) and (bp > 37000000) and (bp < 43000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==11) and (bp > 46000000) and (bp < 57000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==11) and (bp > 87000000) and (bp < 91000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==12) and (bp > 33000000) and (bp < 40000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==12) and (bp > 109000000) and (bp < 112000000):
-            snp_out.write(snp + ' longLD_region\n')
-        elif (chrom==20) and (bp > 32000000) and (bp < 35000000):
-            snp_out.write(snp + ' longLD_region\n')
+            
 
 snp_in.close()
 
