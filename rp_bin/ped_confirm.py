e57bd4184e9d0c69131b8069a99e02822031b991
rkwalters <rwalters@broadinstitute.org>
2015-11-13 12:32:07 -0500

    arg checks, primus pedigree reconstruction

--- rp_bin/ped_confirm.py
+++ rp_bin/ped_confirm.py
@@ -32,7 +32,7 @@ if not (('-h' in sys.argv) or ('--help' in sys.argv)):
 
 ### load requirements
 # import os
-# import subprocess
+import subprocess
 import argparse
 # from string import ascii_uppercase
 # from glob import glob
@@ -58,17 +58,78 @@ parser = argparse.ArgumentParser(prog='ped_confirm.py',
 args = parser.parse_args()
 
 
+# print settings
+print 'Using settings:'
+print '--input-ibd '+str(args.input_ibd)
+print '--bfile '+str(args.bfile)
+print '--out '+str(args.out)
+print '--format '+str(args.format)
+print '--min-rel '+str(args.min_rel)
+
+# verify input files exist
+assert os.path.isfile(args.input_ibd), "IBD/relatedness file does not exist (%r)" % args.input_ibd
+assert os.path.isfile(str(args.bfile)+'.fam'), "Plink fam file does not exist (%s)" % str(args.bfile)+'.fam'
+
+# test executables
+test_exec(args.primus_ex, 'PRIMUS')
+test_exec(args.findped_ex, 'PRIMUS pedigree matching script')
+
+# unzip relatedness file if needed
+if args.input_ibd.endswith('.gz'):
+    print 'Unzipping IBD relatedness file'
+    ibd_txtfile = str(args.input_ibd) + '.txt'
+    subprocess.check_call(['gunzip','-c',str(args.input_ibd),'>',str(ibd_txtfile)])
+else:
+    ibd_txtfile = str(args.input_ibd)
+
+assert os.path.isfile(ibd_txtfile), "Failed to extract IBD/relatedness file (%r)" % args.input_ibd
 
 
 
-# PRIMUS
-# --input FILE=(reap, zip not allowed) IBD0=6 IBD1=7 IBD2=8 RELATEDNESS=9
-# --rel_threshold .09375
-# --output_dir
-# --no_IMUS
-# --sexes FILE=.fam SEX=5 
-# --max_gens 5
-# --affections FILE=fam AFFECTION=6 
+print '\n'
+print '############'
+print 'Begin!'
+print '############'
+
+
+#############
+print '\n...Constructing pedigrees from IBD/relatedness...'
+# - Run using primus
+# - args depend on ibd file format (default here: reap)
+#############
+
+primus_peds_log = open(str(args.out)+'.primus_peds.log', 'w')
+
+if(args.format=='reap'):
+    pr_input_text = '--input '+str(ibd_textfile)+' IBD0=6 IBD1=7 IBD2=8 RELATEDNESS=9'
+else:
+    raise ValueError('Unsupported IBD file format (%s)' % str(args.format))
+
+subprocess.check_call([str(args.primus_ex),
+                       str(pr_input_text),
+                       '--rel_threshold',str(args.min_rel),
+                       '--no_IMUS',
+                       '--max_gens',str(5),
+                       '--sexes',str(args.bfile)+'.fam','SEX=5',
+                       '--affections',str(args.bfile)+'.fam','AFFECTION=6',
+                       '--output_dir',str(args.out)+'_primus_peds'],
+                       stderr=subprocess.STDOUT,
+                       stdout=primus_peds_log)
+
+primus_pr_log.close()
+
+
+
+#############
+print '\n...Preparing fam files...'
+# - Format fam files for PRIMUS pedigree matching
+# - Requires separate fam file per FID
+# - Sibs must have parents
+# TODO: find general solution for adding dummy parents
+# - currently: add father/mother if >1 IID in FID and no IIDs have listed parents
+#############
+
+
 
 # awk '{print $1,$2,"fid"$1"father","fid"$1"mother",$5,$6 > "pitt_clean_pruned_famex_fid"$1".fam"}' pitt_clean_pruned_famex.fam
 
